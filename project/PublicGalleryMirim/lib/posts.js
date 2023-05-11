c772740e143eb372f0ff8dc7cba9a0e7871dd4bf
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2023-05-11 22:32:12 +0900

    [feat] #1 ~9.4
    - 실습 진행

--- project/PublicGalleryMirim/lib/posts.js
+++ project/PublicGalleryMirim/lib/posts.js
@@ -2,6 +2,8 @@ import firestore from '@react-native-firebase/firestore';
 
 const postsCollection = firestore().collection('posts');
 
+export const PAGE_LIMIT = 3;
+
 export function createPost({user, photoURL, description}) {
   return postsCollection.add({
     user,
@@ -9,4 +11,49 @@ export function createPost({user, photoURL, description}) {
     description,
     createdAt: firestore.FieldValue.serverTimestamp(),
   });
+}
+
+export async function getPosts() {
+  const snapShot = await postsCollection
+    .orderBy('createdAt', 'desc')
+    .limit(PAGE_LIMIT)
+    .get();
+  const posts = snapShot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return posts;
+}
+
+export async function getOlderPosts(id) {
+  const cursorDoc = await postsCollection.doc(id).get();
+  const snapShot = await postsCollection
+    .orderBy('createdAt', 'desc')
+    .startAfter(cursorDoc)
+    .limit(PAGE_LIMIT)
+    .get();
+
+  const posts = snapShot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return posts;
+}
+
+export async function getNewerPosts(id) {
+  const cursorDoc = await postsCollection.doc(id).get();
+  const snapShot = await postsCollection
+    .orderBy('createdAt', 'desc')
+    .endBefore(cursorDoc)
+    .limit(PAGE_LIMIT)
+    .get();
+
+  const posts = snapShot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return posts;
 }
--- project/PublicGalleryMirim/screens/FeedScreen.js
+++ project/PublicGalleryMirim/screens/FeedScreen.js
@@ -1,8 +1,89 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
+import {PAGE_LIMIT, getNewerPosts, getOlderPosts, getPosts} from '../lib/posts';
+import PostCard from '../components/PostCard';
 
 function FeedScreen() {
-  return <View />;
+  const [posts, setPosts] = useState(null);
+  const [noMorePost, setNoMorePost] = useState(false);
+  const [refreshing, setRefereshing] = useState(false);
+
+  useEffect(() => {
+    getPosts().then(setPosts);
+  }, []);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length > PAGE_LIMIT) {
+      return;
+    }
+
+    const lastPost = posts[posts.length - 1];
+    const olderPost = await getOlderPosts(lastPost.id);
+    if (olderPost.length < PAGE_LIMIT) {
+      setNoMorePost(true);
+    }
+
+    setPosts(posts.concat(olderPost));
+  };
+
+  const onRefresh = async () => {
+    if (!posts || posts.length === 0 || refreshing) {
+      return;
+    }
+
+    const firstPost = posts[0];
+    setRefereshing(true);
+    const newerPosts = await getNewerPosts(firstPost.id);
+    setRefereshing(false);
+    if (newerPosts.length === 0) {
+      return;
+    }
+
+    setPosts(newerPosts.concat(posts));
+  };
+
+  return (
+    <FlatList
+      data={posts}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+        )
+      }
+      refreshControl={
+        <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+      }
+    />
+  );
 }
+
+const renderItem = ({item}) => (
+  <PostCard
+    createdAt={item.createdAt}
+    description={item.description}
+    id={item.id}
+    user={item.user}
+    photoURL={item.photoURL}
+  />
+);
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 48,
+  },
+  spinner: {
+    height: 64,
+  },
+});
 
 export default FeedScreen;
